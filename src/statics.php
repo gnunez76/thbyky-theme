871ada67266d734dd0eb053b81b5cb19a4cb288b
Gustavo Núñez <gnunez@thinkingsize.com>
2018-04-01 17:59:37 +0200

    Done

--- src/statics.php
+++ src/statics.php
@@ -5,7 +5,7 @@ add_filter('style_loader_src', function ($url) {
 
     if (preg_match('#^'.site_url().'#', $url)) {
 
-        $url = preg_replace ('#^'.site_url().'#','http://ykys2.mrjack.es', $url);
+        $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
     }
 
     return $url;
@@ -14,7 +14,7 @@ add_filter('style_loader_src', function ($url) {
 add_filter('script_loader_src', function ($url) {
 
     if (preg_match('#^'.site_url().'#', $url)) {
-        $url = preg_replace ('#^'.site_url().'#','http://ykys2.mrjack.es', $url);
+        $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
     }
 
     return $url;
@@ -23,7 +23,7 @@ add_filter('script_loader_src', function ($url) {
 add_filter ( 'get_site_icon_url', function ($url) {
 
     if (preg_match('#^'.site_url().'#', $url)) {
-        $url = preg_replace ('#^'.site_url().'#','http://ykys2.mrjack.es', $url);
+        $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
     }
 
     return $url;
@@ -32,6 +32,6 @@ add_filter ( 'get_site_icon_url', function ($url) {
 // Url de STATIC en media
 add_filter('wp_get_attachment_url', function ($url)  {
 
-    $url = preg_replace ('#^'.site_url().'#','http://ykys2.mrjack.es', $url);
+    $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
     return $url;
 }, 10, 1);
