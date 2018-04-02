6b88ed3d6fd666e5ec574d6066145dbe7b04dfc6
Gustavo Núñez <gnunez@thinkingsize.com>
2018-04-02 21:08:56 +0200

    cambio estaticos

--- src/statics.php
+++ src/statics.php
@@ -1,11 +1,37 @@
 <?php
 
 
+function thbgetDomain ($url) {
+
+    $staticHosts = array ('https://ykys1.eirendor.com','https://ykys2.eirendor.com');
+
+    $url = preg_replace ('#^'.site_url().'#','', $url);
+
+    if (!empty ($url)) {
+        if (count($staticHosts) > 0) {
+
+            $_aux = abs(crc32($url)) % count($staticHosts);
+            if (!empty ($staticHosts[$_aux])) {
+                $url = $staticHosts[$_aux] . $url;
+            } else {
+                $url = home_url() . $url;
+            }
+
+        } else {
+            $url = home_url() . $url;
+        }
+
+    }
+
+    return $url;
+}
+
 add_filter('style_loader_src', function ($url) {
 
     if (preg_match('#^'.site_url().'#', $url)) {
 
-        $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
+//        $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
+        $url = thbgetDomain($url);
     }
 
     return $url;
@@ -14,7 +40,9 @@ add_filter('style_loader_src', function ($url) {
 add_filter('script_loader_src', function ($url) {
 
     if (preg_match('#^'.site_url().'#', $url)) {
-        $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
+
+//        $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
+        $url = thbgetDomain($url);
     }
 
     return $url;
@@ -23,7 +51,9 @@ add_filter('script_loader_src', function ($url) {
 add_filter ( 'get_site_icon_url', function ($url) {
 
     if (preg_match('#^'.site_url().'#', $url)) {
-        $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
+
+//        $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
+        $url = thbgetDomain($url);
     }
 
     return $url;
@@ -32,6 +62,8 @@ add_filter ( 'get_site_icon_url', function ($url) {
 // Url de STATIC en media
 add_filter('wp_get_attachment_url', function ($url)  {
 
-    $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
+//    $url = preg_replace ('#^'.site_url().'#','https://ykys2.mrjack.es', $url);
+    $url = thbgetDomain($url);
+
     return $url;
 }, 10, 1);
