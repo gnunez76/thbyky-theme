58c0b81b89d80dd9323404aa32ff4baa55964e53
Gustavo Núñez <gnunez@thinkingsize.com>
2018-04-07 19:04:00 +0200

    200

--- src/htmlblock.php
+++ src/htmlblock.php
@@ -5,6 +5,7 @@ if (preg_match('#^/thb-html-block/(?P<type>[^/]+)/#', $_SERVER["REQUEST_URI"], $
     add_filter('show_admin_bar', '__return_false');
     add_action('template_include', function ($template) use ($match) {
 
+        status_header(200);
         switch (strtoupper($match['type'])) {
             case 'FOOTER-BLOCK':
                 $template = __DIR__ . '/../html-block-footer.php';
