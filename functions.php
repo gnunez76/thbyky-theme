67dfbac3e7112546921c48f38d8d8b0785e5002a
Gustavo Núñez <gnunez@thinkzz.com>
2018-05-21 20:49:38 +0200

    política registro

--- functions.php
+++ functions.php
@@ -39,3 +39,18 @@ add_action('wp_enqueue_scripts', function () {
     wp_dequeue_script( 'lolfmk-google-maps' );
     wp_deregister_script( 'lolfmk-google-maps' );
 });
+
+
+function wooc_add_field_to_registration(){
+    wc_get_template( 'checkout/terms.php' );
+}
+add_action( 'woocommerce_register_form', 'wooc_add_field_to_registration' );
+
+
+function wooc_validation_registration( $errors, $username, $password, $email ){
+    if ( empty( $_POST['terms'] ) ) {
+        throw new Exception( __( 'Debes aceptar los términos y condiciones', 'woocommerce' ) );
+    }
+    return $errors;
+}
+add_action( 'woocommerce_process_registration_errors', 'wooc_validation_registration', 10, 4 );
